06681d8fe9e71390aee0aea29ad98ef5fa6622bb
sonbk040901 <leducson007@gmail.com>
2024-07-11 04:14:06 +0700

    update

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -3,7 +3,7 @@ import Title from "antd/es/typography/Title";
 const DashBoard = () => {
   return (
     <div className="grid place-items-center h-full">
-      <Title editable={{ autoSize: true }}>DashBoard</Title>
+      <Title className="animate-pulse">Chào mừng bạn đến với BKSAFE</Title>
     </div>
   );
 };
